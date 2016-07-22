1eb10b73ced7de286cab19e04db43e5d73e618f3
zakary <94zakary@gmail.com>
2016-07-21 17:49:17 -0700

    added static page controller for /home and /about

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, :only => [:show]
   resources :pieces
-  root 'pieces#index'
+  root 'pages#home'
+
+  # using a controller to server static pages
+  controller :pages do
+    get :home
+    get :about
+  end
+
 end
