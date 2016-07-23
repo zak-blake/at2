95a2a45f184b4f74e39879b58b3570e844baf10a
zakary <94zakary@gmail.com>
2016-07-22 22:15:31 -0700

    added avatar to user and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :avatar, styles: { large: "600x600>", medium: "300X300", thumb: "100x100>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723013323) do
+ActiveRecord::Schema.define(version: 20160723034209) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
@@ -51,6 +51,10 @@ ActiveRecord::Schema.define(version: 20160723013323) do
     t.datetime "updated_at",                          null: false
     t.string   "full_name"
     t.text     "bio"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
