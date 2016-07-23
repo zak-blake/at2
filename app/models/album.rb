a3ba2e3d82eb3ca27f56ede699a25e4fc9fe8367
zakary <94zakary@gmail.com>
2016-07-22 20:00:02 -0700

    Added album images

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,7 @@
 class Album < ApplicationRecord
   belongs_to :user
   has_many :pieces
+
+  has_attached_file :image, styles: { large: "600x600>", medium: "300X300", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722235147) do
+ActiveRecord::Schema.define(version: 20160723013323) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "pieces", force: :cascade do |t|
