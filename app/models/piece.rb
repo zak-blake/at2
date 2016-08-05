58b2636e125d6869a681091d9226bd94a43d2274
zakary <94zakary@gmail.com>
2016-08-05 16:26:15 -0700

    fixed accidental change to album view

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,6 +1,6 @@
 class Piece < ApplicationRecord
   belongs_to :album
 
-  has_attached_file :image, styles: { large: "600x600", medium: "300X300", thumb: "100x100>", grid_view: "750x450#" }
+  has_attached_file :image, styles: { large: "600x600", medium: "300X300", thumb: "100x100", grid_view: "750x450#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
