6c4c2722da1c8fd701f983aa6ca3060d37864591
zakary <94zakary@gmail.com>
2016-09-03 21:41:47 -0700

    scaled full_size images down slightly

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :avatar, styles: { large: "1782x918>", thumb: "300X300>" }
+  has_attached_file :avatar, styles: { large: "1485x765>", thumb: "300X300>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
