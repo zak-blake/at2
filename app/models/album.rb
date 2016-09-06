c08fe6909c09ac4d42cdba49306b448c177092dc
zakary <94zakary@gmail.com>
2016-09-05 18:36:23 -0700

    user show revamp complete to display pieces instead of album thumbnails

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,4 @@ class Album < ApplicationRecord
   belongs_to :user
   has_many :pieces
 
-  has_attached_file :image, styles: { show_album: "700x300#" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,10 +18,6 @@ ActiveRecord::Schema.define(version: 20160905002828) do
     t.integer  "user_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
     t.integer  "priority"
   end
 
